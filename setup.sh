b31b4629112b72b49c20536ec88cb3bdc8ba5fe4
Ubuntu <artemis@crawler.tu3xmo2ljkmu5pbvf35wpngr2b.rx.internal.cloudapp.net>
2023-03-19 00:25:25 +0000

    added setup .sh and updated database.py

--- setup.sh
+++ setup.sh
@@ -5,3 +5,6 @@ pip install -r requirements.txt
 sudo apt install mysql-server -y
 sudo mysql_secure_installation
 python3 database.py
+sudo apt install adminer -y
+sudo a2enconf adminer
+sudo systemctl reload apache2
