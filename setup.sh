766868ed6db3a9ad4728f77ae91555dbbd54f16d
Ubuntu <artemis@crawler.kwtwb41ihkbedeaji5keoiex5d.dohx.internal.cloudapp.net>
2023-03-19 00:37:59 +0000

    repaierd setup.sh

--- setup.sh
+++ setup.sh
@@ -3,7 +3,8 @@ sudo apt upgraed -y
 sudo apt install python3-pip -y
 pip install -r requirements.txt
 sudo apt install mysql-server -y
-sudo mysql_secure_installation
+#sudo mysql 
+#ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'SetRootPasswordHere';
 python3 database.py
 sudo apt install adminer -y
 sudo a2enconf adminer
