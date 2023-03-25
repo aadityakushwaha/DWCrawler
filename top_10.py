e43ba588efd3b3455a68525d018c16bb72c79c99
ar005 <108510728+ar005@users.noreply.github.com>
2023-03-25 05:56:50 +0530

    added top ten url feature

---
+++ top_10.py
@@ -0,0 +1,29 @@
+import mysql.connector
+
+# Connect to the MySQL database
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password="Girlactor@77",
+  database="Crawler"
+)
+
+# Create a cursor to execute SQL queries
+mycursor = mydb.cursor()
+
+# Execute SQL query to fetch data and sort it by count column in descending order
+mycursor.execute("SELECT * FROM onion_urls ORDER BY count DESC LIMIT 60")
+
+# Get the results and print them out
+results = mycursor.fetchall()
+non_wiki_count=0
+for result in results:
+
+    if ("wikipedia" not in str(result)) & (non_wiki_count<10):
+        non_wiki_count+=1
+        print(result[1])
+
+
+mydb.close()
+
+
