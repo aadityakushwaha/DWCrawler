dd5bd2bc423c5157c1b84ab75cc6722a58935ff2
Aaditya Kushwaha <84792615+aadityakushwaha@users.noreply.github.com>
2023-03-25 13:53:17 +0530

    Update crawler.py

--- crawler.py
+++ crawler.py
@@ -16,7 +16,7 @@ def deep_scan(url):
     mysql_config = {
         "host": "localhost",
         "user": "root",
-        "password": "Girlactor@77",
+        "password": "",
         "database": "Crawler"
     }
     
